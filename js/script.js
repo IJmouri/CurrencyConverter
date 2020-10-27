8bf70922cb77fadd8617b8593ee0d0ceebbb2ea7
Israt-Jahan-Mouri <mouri2018@gmail.com>
2020-10-27 13:52:12 +0600

    more options added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // variables
+
 let selectFromCurrency = document.getElementById('select-from-currency');
 let selectToCurrency = document.getElementById('select-to-currency');
 let fromCurrencyInput = document.querySelector('#from-currency-input');
@@ -10,33 +11,35 @@ let toCurrencyText = document.querySelector('.to-currency-text');
 // event listeners
 
 
-selectFromCurrency.addEventListener('change', calculate);
-fromCurrencyInput.addEventListener('input', calculate);
-selectToCurrency.addEventListener('change', calculate);
+selectFromCurrency.addEventListener('change', calculateCurrency);
+fromCurrencyInput.addEventListener('input', calculateCurrency);
+selectToCurrency.addEventListener('change', calculateCurrency);
 
 function showFromSelectList(){
     
     $('#select-from-currency').ddslick({
         width:'100%',
+        maxHeight: "300px",
         imagePosition: 'left',
         // selectText: 'select country',
         onSelected: function(selectedData){
-            calculate();
+            calculateCurrency();
         }
-
     });
+
 }
 function showToSelectList(){
     
     $('#select-to-currency').ddslick({
         width:'100%',
         imagePosition: 'left',
+        maxHeight: "300px",
         // selectText: 'select country',
         onSelected: function(selectedData){
-            calculate();
+            calculateCurrency();
         }
-
     });
+
 }
 
 showFromSelectList();
@@ -44,33 +47,8 @@ showToSelectList();
 
 // calculate currency
 
-function calculate() {
-    // ddslick();
-    // $('#select-from-currency').ddslick({
-    //     width:'50%',
-    //     imagePosition: 'left',
-    //     selectText: 'select country',
-    //     onSelected: function(data){
-    //         // $('#to-currency-value').html(data.selected);
-    //         let request = new XMLHttpRequest();
-    //         request.open('GET', 'https://api.exchangerate-api.com/v4/latest/' + selectFromCurrency.value, true);
-    //         request.send();
-        
-    //         request.onreadystatechange = function handleRequest() {
-    //             if (request.readyState === 4 && request.status === 200) {
-        
-    //                 let data = JSON.parse(request.responseText);
-    //                 let selectToCurrencyValue = selectToCurrency.value;
-    //                 let rate = data.rates[selectToCurrencyValue];
-        
-    //                 toCurrencyValueShow.innerHTML =  (fromCurrencyInput.value * rate ).toFixed(4);
-    //                 fromCurrencyTextInput.innerHTML = fromCurrencyInput.value;
-    //                 fromCurrencyText.innerHTML = selectFromCurrency.value;
-    //                 toCurrencyText.innerHTML = selectToCurrency.value;
-    //             }
-    //         }
-    //     }
-    // });
+function calculateCurrency() {
+    
     let request = new XMLHttpRequest();
     request.open('GET', 'https://api.exchangerate-api.com/v4/latest/' + selectFromCurrency.value, true);
     request.send();
@@ -82,7 +60,7 @@ function calculate() {
             let selectToCurrencyValue = selectToCurrency.value;
             let rate = data.rates[selectToCurrencyValue];
 
-            toCurrencyValueShow.innerHTML =  (fromCurrencyInput.value * rate ).toFixed(4);
+            toCurrencyValueShow.innerHTML =  (fromCurrencyInput.value * rate ).toFixed(2);
             fromCurrencyTextInput.innerHTML = fromCurrencyInput.value;
             fromCurrencyText.innerHTML = selectFromCurrency.value;
             toCurrencyText.innerHTML = selectToCurrency.value;
@@ -92,4 +70,4 @@ function calculate() {
    
 }
 
-calculate();
+calculateCurrency();
